a65f56c9ca3efeda6ddc1fb1a4a8782e5f8f0892
vashaty <v_toncer@utb.cz>
2021-02-01 14:51:14 +0100

    Added splash screen

--- app/src/main/java/cz/utb/fai/sportsdb/MainActivity.java
+++ app/src/main/java/cz/utb/fai/sportsdb/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
